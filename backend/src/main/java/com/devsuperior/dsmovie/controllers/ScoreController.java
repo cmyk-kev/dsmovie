df92f336f5498b97437372b54a441fa8eb303be4
Kevin da Silva Felix <kevinsfelix@outlook.com>
2022-03-11 22:07:42 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsmovie.controllers;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.services.MovieService;
+import com.devsuperior.dsmovie.services.ScoreService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+
+ @Autowired 
+  private ScoreService service; //
+
+  @PutMapping
+  public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
+    MovieDTO movieDTO = service.saveScore(dto);
+    return movieDTO;
+  }
+
+}
